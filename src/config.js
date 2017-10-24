b2dcedebe6bc88a5ae5271cc2203466a25f1ab33
Lance <whatl3y@gmail.com>
2017-10-24 08:46:19 -0400

    Update config

--- src/config.js
+++ src/config.js
@@ -1,21 +1,21 @@
 export default {
-  filepath: process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'],
+  filepath: process.env.TXR_PATH || process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'],
 
   server: {
-    port: process.env.PORT || 8000,
-    web_concurrency: process.env.WEB_CONCURRENCY || 1,
-    host: process.env.HOSTNAME || "http://localhost:8000"
+    port:     process.env.PORT || 8000,
+    web_conc: process.env.WEB_CONCURRENCY || 1,
+    host:     process.env.HOSTNAME || "https://txr.herokuapp.com"
   },
 
   logger: {
     options: {
-      name: process.env.APP_NAME || "file-handoff",
+      name: process.env.APP_NAME || "txr",
       level: process.env.LOGGING_LEVEL || "info",
       stream: process.stdout
       /*streams: [
         {
           level: process.env.LOGGING_LEVEL || "info",
-          path: path.join(__dirname, "logs", "file-handoff.log")
+          path: path.join(__dirname, "logs", "txr.log")
         }
       ]*/
     }
