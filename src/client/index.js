83ca2a34dca6ed260bbed0b1d7da31c8bbd84e6b
Lance <whatl3y@gmail.com>
2017-10-25 22:22:14 -0400

    Cleanup spacing

--- src/client/index.js
+++ src/client/index.js
@@ -6,11 +6,11 @@ const argv = minimist(process.argv.slice(2))
 const [ first, second, third ] = argv._
 
 ;(async function go() {
-  const command         = argv.c || argv.command || first
-  const file            = argv.f || argv.file || second
-  const user            = argv.u || argv.username || third || second
-  const auth            = argv.a || argv.auth
-  const host            = argv.h || argv.host
+  const command = argv.c || argv.command || first
+  const file    = argv.f || argv.file || second
+  const user    = argv.u || argv.username || third || second
+  const auth    = argv.a || argv.auth
+  const host    = argv.h || argv.host
 
   if (command && allCommands[command]) {
     await allCommands[command]({ file, user, auth, host })
